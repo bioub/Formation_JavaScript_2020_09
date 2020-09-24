4de67b9d26a790992bf5150efa1997931c625e2d
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-09-24 14:09:34 +0200

    Exercice express

--- Node.js/02-express.js
+++ Node.js/02-express.js
@@ -31,6 +31,19 @@ const todos = [
   },
 ];
 
+const users = [
+   {
+     id: 1,
+     username: 'romain',
+     password: '123456'
+   },
+   {
+    id: 2,
+    username: 'eric',
+    password: 'soleil'
+  }
+]
+
 app.get('/api/todos', (req, res) => {
   res.json(todos);
 });
@@ -47,6 +60,31 @@ app.get('/api/todos/:todoId', (req, res) => {
 
   res.json(todo);
 });
+
+app.post('/api/todos', express.json(), (req, res) => {
+  const todo = req.body;
+
+  todo.id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+
+  todos.push(todo);
+
+  res.status(201); // CREATED
+  res.json(todo);
+});
+
+// Exercice 1
+// Sur une requête GET /api/users
+// Repondre en JSON la liste des users sans le password
+
+// Exercice 2
+// Sur une requête POST /api/users
+// Repondre en JSON le user créé avec un id sans le password
+// et le stocker dans le tableau users
+
+// Exercice 3
+// Sur une requête DELETE /api/todos/:todoId
+// Supprimer la todo correspondante du tableau (relire la doc MDN sur les tableaux)
+// Répondre la todo supprimée
 
 // server.on('connection', (socket) => {
 //   socket.pipe(process.stdout); // redirige la sortie de socket en entrée du terminal
